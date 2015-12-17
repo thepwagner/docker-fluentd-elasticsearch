00d5c6ea6d8bb5d2546d0b40fcbe9d4ff00b1fc4
Peter Wagner <pwagner@pebble.com>
2015-12-17 07:33:08 -0500

    Good enough journalctl handling
    Normalizing messages from different transports, making sure everything
    is in a format ElasticSearch can index nicely.

--- fluentd-entry.sh
+++ fluentd-entry.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+TIMEOUT=2
+
+export AZ=$(curl -s -m ${TIMEOUT} http://169.254.169.254/latest/meta-data/placement/availability-zone)
+export SECURITY_GROUP=$(curl -s -m ${TIMEOUT} http://169.254.169.254/latest/meta-data/security-groups)
+export INSTANCE_ID=$(curl -s -m ${TIMEOUT} http://169.254.169.254/latest/meta-data/instance-id)
+export INSTANCE_TYPE=$(curl -s -m ${TIMEOUT} http://169.254.169.254/latest/meta-data/instance-type)
+export INSTANCE_IP=$(curl -s -m ${TIMEOUT} http://169.254.169.254/latest/meta-data/local-ipv4)
+
 # Expand template:
 eval "echo \"$(cat /fluentd/etc/fluent.conf)\"" \
 	> /tmp/fluent.conf
